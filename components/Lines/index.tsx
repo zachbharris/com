c2ff948b4fcbe37fae3b3174aff1233961f607b6
Zach Harris <zachbharris94@gmail.com>
2021-03-04 22:25:43 -0600

    basic site to hold until later

--- components/Lines/index.tsx
+++
@@ -1,5 +0,0 @@
-import styles from 'styles/Line.module.css'
-
-export default function Lines() {
-	return <div className={styles.lines} />
-}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import Head from 'next/head'
-import Lines from 'components/Lines'
-import styles from '../styles/Home.module.css'
+
+// styles
+import styles from 'styles/Home.module.css'
 
 export default function Home() {
 	return (
 		<div className={styles.container}>
 			<Head>
-				<title>Create Next App</title>
+				<title>Zach Harris</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<Lines />
+			<h1>Zach Harris</h1>
+			<span>Software Engineer in St. Louis</span>
 		</div>
 	)
 }
